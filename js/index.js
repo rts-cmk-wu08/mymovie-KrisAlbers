edb044b1dec429c7e994a10e0716a625ca79badd
KristineAlbers <1259@rts-365.dk>
2022-09-13 20:41:35 +0200

    index white

---
+++ js/index.js
@@ -0,0 +1,96 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let MymoviesMode = document.createElement("section");
+  MymoviesMode.classList.add("Mymovies");
+  MymoviesMode.innerHTML = `
+  <div class="container1 blog">
+      <p></p>
+      <h1>MyMovies</h1>
+      <label class="switch" for="toggle-btn">
+      <input id="toggle-btn" type="checkbox">
+      <span class="slider round"></span>
+      </label>
+    </div>`;
+  document.body.append(MymoviesMode);
+
+  let NowShowing = document.createElement("section");
+  NowShowing.classList.add("headerNowShowing");
+  NowShowing.innerHTML = `
+  <div class="container1">
+      <h1>Now Showing</h1>
+      <button class="btnGrey">See more</button>
+    </div>`;
+  document.body.append(NowShowing);
+
+  let sectionNowShowingContainer = document.createElement("section");
+  sectionNowShowingContainer.classList.add("sectionNowShowing");
+  document.body.append(sectionNowShowingContainer);
+
+  fetch(
+    `https://api.themoviedb.org/3/movie/now_playing?api_key=02edc4c44ad4486b6397687549f262c7&language=en-US&page=1`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      data.results.forEach((result) => {
+        let NowShowingLink = document.createElement("a");
+        NowShowingLink.setAttribute("href", `detail.html?id=${result.id}`);
+        NowShowingLink.classList.add("NowShowingLink");
+        NowShowingLink.innerHTML = `
+          <img class="nowShowingImg" src="https://image.tmdb.org/t/p/w500${result.poster_path}" alt="${result.title}">
+          <h2 class="">${result.title}</h2>
+          <p>${result.popularity}/10 IMDb</p>
+          `;
+        sectionNowShowingContainer.append(NowShowingLink);
+      });
+    });
+
+  let popular = document.createElement("section");
+  popular.classList.add("headerNowShowing");
+  popular.innerHTML = `
+    <div class="container1">
+        <h1>Popular</h1>
+        <button class="btnGrey">See more</button>
+      </div>`;
+  document.body.append(popular);
+
+
+  fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=02edc4c44ad4486b6397687549f262c7&language=en-US&page=1`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      data.results.forEach((result) => {
+        let popularLink = document.createElement("a");
+        popularLink.setAttribute("href", `detail.html?id=${result.id}`);
+        popularLink.classList.add("popularLink");
+        popularLink.innerHTML = `
+          <img class="nowShowingImg" src="https://image.tmdb.org/t/p/w500${result.poster_path}" alt="${result.title}">
+          <div class="containerPopular">
+          <h2 class="">${result.title}</h2>
+          <p>${result.popularity}/10 IMDb</p>
+          <div>
+          `;
+        document.body.append(popularLink);
+      });
+    });
+
+
+  /*
+          <p>${result.popularity}.toFixed(10)/10 IMDb</p>
+
+  let blogContainer = document.querySelector(".blog");
+  let toggleBtn = document.querySelector("#toggle-btn");
+  let switchTheme = () => {
+    blogContainer.classList.toggle("dark");
+  };
+  toggleBtn.addEventListener("click", switchTheme);
+
+
+
+
+
+*/
+});
